1690ce6a20bd0e45c7825f1dbd4255e4f85c139e
Carlos Anriquez <canriquez@gmail.com>
2020-05-13 13:57:52 -0300

    finishing basic association tests in console. adjusting naming done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   has_many :authored_courses, foreign_key: 'author_id', class_name: "Group"
   has_many :enrolled_courses, foreign_key: 'student_id', class_name: "Enroll"
-  has_many :taugth_sessions, foreign_key: 'teacher_id', class_name: "Transaction"
+  has_many :taught_sessions, foreign_key: 'teacher_id', class_name: "Transaction"
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
