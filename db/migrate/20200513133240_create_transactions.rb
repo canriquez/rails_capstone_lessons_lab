593c01247375aaedc12467d2c3e731571eb54a9b
Carlos Anriquez <canriquez@gmail.com>
2020-05-16 05:31:26 -0300

    fix migrations issues failing at heroku migration

--- db/migrate/20200513133240_create_transactions.rb
+++ db/migrate/20200513133240_create_transactions.rb
@@ -5,10 +5,11 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
       t.integer :status
       t.integer :minutes
       t.date    :accdate
+      t.integer :sitting_student_id, null: true, foreign_key: true
+      t.integer :course_taught_id, null: true, foreign_key: true
 
       t.timestamps
     end
-    t.integer :sitting_student_id, null: true, foreign_key: true
-    t.integer :course_taught_id, null: true, foreign_key: true
+
   end
 end
