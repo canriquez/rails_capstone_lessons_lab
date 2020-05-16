afe8d0ef10ef476bc595d3fbb1fe8481845e281b
Carlos Anriquez <canriquez@gmail.com>
2020-05-16 05:28:48 -0300

    fix migrations issues failing at heroku migration

--- db/migrate/20200513110914_create_enrolls.rb
+++ db/migrate/20200513110914_create_enrolls.rb
@@ -3,10 +3,9 @@ class CreateEnrolls < ActiveRecord::Migration[5.2]
     create_table :enrolls do |t|
       t.integer :student_id
       t.integer :course_id
-      add_foreign_key :enrolls, :users, column: :student_id, primary_key: "id"
-      add_foreign_key :enrolls, :groups, column: :course_id, primary_key: "id"
-
       t.timestamps
     end
+    add_foreign_key :enrolls, :users, column: :student_id, primary_key: "id"
+    add_foreign_key :enrolls, :groups, column: :course_id, primary_key: "id"
   end
 end
--- db/migrate/20200513133240_create_transactions.rb
+++ db/migrate/20200513133240_create_transactions.rb
@@ -2,13 +2,13 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
       t.integer :teacher_id
-      t.integer :sitting_student_id, null: true, foreign_key: true
-      t.integer :course_taught_id, null: true, foreign_key: true
       t.integer :status
       t.integer :minutes
       t.date    :accdate
 
       t.timestamps
     end
+    t.integer :sitting_student_id, null: true, foreign_key: true
+    t.integer :course_taught_id, null: true, foreign_key: true
   end
 end
